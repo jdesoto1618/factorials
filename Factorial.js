1800b52b0854681678a61c3edae697a18453087b
Joe DeSoto <modulusman@gmail.com>
2017-08-27 11:51:58 -0700

    Fixes string input case

--- Factorial.js
+++ Factorial.js
@@ -1,17 +1,22 @@
 function fact(num) {
-  //Negative values
-  if (num < 0) {
-    return "Factorials are not defined for numbers less than 0"
-  //Input is 0 or 1
-  } else if (num == 0 || num == 1) {
-    return 1
-  //All other values
+  //String or empty input
+  if(typeof(num) == "string") {
+    return "Please use numeric input only."
   } else {
-    //Start at the number before the input, as the number itself is accounted for in line 12. Stop at 2, since multiplying by 1 changes nothing. Decrement by 1 each time.
-    for (var i=num-1; i > 1; i--) {
-      //Multiply the numebr by consecutively smaller integers
-      num *= i
+    //Negative values
+    if (num < 0) {
+      return "Factorials are not defined for numbers less than 0"
+    //Input is 0 or 1
+    } else if (num == 0 || num == 1) {
+      return 1
+    //All other values
+    } else {
+      //Start at the number before the input, stop at 2, decrement by 1
+      for (var i=num-1; i > 1; i--) {
+        //Multiply the numebr by consecutively smaller integers
+        num *= i
+      }
     }
+    return num
   }
-  return num
 }
